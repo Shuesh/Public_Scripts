05b9f0f7d6f86cd20453730632a61dcf2dfd9f57
Shuesh <holtz185@umn.edu>
2021-12-12 00:52:26 -0600

    Day_12 Complete

--- AOC_2021/Day_12/Solution_12_p1.py
+++ AOC_2021/Day_12/Solution_12_p1.py
@@ -1,34 +1,61 @@
 import re
 
+
+valid_paths = []
+
 def main():
-    input_list = Input_to_List('Day_12/Example_12.txt')
-
-
-
+    input_dict = Input_to_List('Day_12/Input_12.txt')
+    valid_paths = Recursive_Search(input_dict, ['start'])
+    print(len(valid_paths))
+    
 
 def Input_to_List(path):
-    file = open(path, 'r')
-    line = list(file.readline().strip())
-    octopus_line = []
-    for octopus in line:
-        octopus_line.append(int(octopus))
+    #Gets all caves
+    match_string = r"[a-zA-Z]+"
 
-    return_list = []
+    file = open(path, 'r')
+    line = file.readline()
+    caves = re.findall(match_string, line)
+
+    return_dict = {}
 
     while True:
         if not line:
             break
         else:
-            return_list.append(octopus_line)
-            line = list(file.readline().strip())
-            octopus_line = []
-            for octopus in line:
-                octopus_line.append(int(octopus))
+            try:
+                return_dict[caves[0]].append(caves[1])
+            except:
+                return_dict[caves[0]] = [caves[1]]
+            
+            try:
+                return_dict[caves[1]].append(caves[0])
+            except:
+                return_dict[caves[1]] = [caves[0]]
 
-    return return_list
+            line = file.readline()
+            caves = re.findall(match_string, line)            
+
+    return return_dict
 
 
 
+#Depth first
+def Recursive_Search(input_dict, path):
+    if path[-1] == 'end':
+        return path
+    else:
+        for cave in input_dict[path[-1]]:
+            if cave[0].isupper() or (cave[0].islower() and cave not in path):
+                temp = path + [cave]
+                finished_path = Recursive_Search(input_dict, temp)
+                if finished_path != None and finished_path != [] and finished_path[-1] == 'end':
+                    valid_paths.append(finished_path)
+    
+    return valid_paths
+
+
+    
 
 if __name__ == '__main__':
     main()
--- AOC_2021/Day_12/Solution_12_p2.py
+++ AOC_2021/Day_12/Solution_12_p2.py
@@ -1,34 +1,73 @@
 import re
 
+
+valid_paths = []
+
 def main():
-    input_list = Input_to_List('Day_12/Example_12.txt')
+    input_dict = Input_to_List('Day_12/Input_12.txt')
+    valid_paths = Recursive_Search(input_dict, ['start'], False)
+    print(len(valid_paths))
 
-
-
+    
 
 def Input_to_List(path):
-    file = open(path, 'r')
-    line = list(file.readline().strip())
-    octopus_line = []
-    for octopus in line:
-        octopus_line.append(int(octopus))
+    #Gets all caves
+    match_string = r"[a-zA-Z]+"
 
-    return_list = []
+    file = open(path, 'r')
+    line = file.readline()
+    caves = re.findall(match_string, line)
+
+    return_dict = {}
 
     while True:
         if not line:
             break
         else:
-            return_list.append(octopus_line)
-            line = list(file.readline().strip())
-            octopus_line = []
-            for octopus in line:
-                octopus_line.append(int(octopus))
+            try:
+                return_dict[caves[0]].append(caves[1])
+            except:
+                return_dict[caves[0]] = [caves[1]]
+            
+            try:
+                return_dict[caves[1]].append(caves[0])
+            except:
+                return_dict[caves[1]] = [caves[0]]
 
-    return return_list
+            line = file.readline()
+            caves = re.findall(match_string, line)        
+
+    for cave in return_dict:
+        return_dict[cave].sort()
+
+    return return_dict
 
 
 
+#Depth first
+def Recursive_Search(input_dict, path, double_small):
+    if path[-1] == 'end':
+        return path
+    else:
+        for cave in input_dict[path[-1]]:
+            double_small = Check_Double_Small(path)
+            if cave[0].isupper() or (cave[0].islower() and cave not in path and cave != 'start') or (not double_small and cave != 'start'):
+                temp = path + [cave]
+                finished_path = Recursive_Search(input_dict, temp, double_small)
+                if finished_path != None and finished_path != [] and finished_path[-1] == 'end':
+                    valid_paths.append(finished_path)
+    
+    return valid_paths
+
+
+def Check_Double_Small(path):
+    for index,cave in enumerate(path):
+        if cave[0].islower():
+            for other in path[index+1:]:
+                if cave == other:
+                    return True
+    return False
+    
 
 if __name__ == '__main__':
     main()
