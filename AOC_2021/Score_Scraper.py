09b2434bcd987aaf84f2f4e349d5d6d7a1ec1cf9
Shuesh <holtz185@umn.edu>
2021-12-14 01:01:31 -0600

    General Stats

--- AOC_2021/Score_Scraper.py
+++ AOC_2021/Score_Scraper.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup as bs
 
 
 def main():
+    user_stats = Get_User_Ranks()
+
     #Get the HTML from the Advent of Code stats page
     page = requests.get('https://adventofcode.com/2021/stats')
     #parse the HTML using BeautifulSoup
@@ -20,19 +22,28 @@ def main():
         percent_two_star = Percent_2_Stars(day[1],day[2])
         clean_stats[index].append(percent_two_star)
 
-    
+    user_stats = Get_User_Ranks()
+    pass
 
 def Percent_2_Stars(two_stars, one_star):
     total = two_stars + one_star
     percent_two_stars = float(two_stars) / float(total)
-    percentage = percent_two_stars * 100
+    percentage = round(percent_two_stars * 100, 2)
 
     return percentage
     
 
-def Get_User_Ranks()
+def OAuth():
+    github_auth = requests.post('https://github.com/login/device/code')
 
 
+def Get_User_Ranks():
+    user_page = requests.get('https://adventofcode.com/2021/leaderboard/self')
+    user_soup = bs(user_page.content, 'html.parser')
+
+    user_stats = user_soup.select("pre text")
+
+    return user_stats
 
 
 if __name__ == '__main__':
