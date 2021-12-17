b4d9255d5df9e6788d54e3962effee020fb8d206
Shuesh <holtz185@umn.edu>
2021-12-16 23:37:19 -0600

    Day_16 Half Complete

--- AOC_2021/Day_16/Solution_16_p1.py
+++ AOC_2021/Day_16/Solution_16_p1.py
@@ -3,18 +3,9 @@ import copy
 
 
 def main():
-    binary_string = Input_to_List('Day_16/Example4_16.txt')
-    remaining_string = binary_string
-
-    version_sum = 0
-    while remaining_string:
-        current_version = Get_Version(remaining_string)
-        version_sum += current_version
-        remaining_string = remaining_string[3:]
-        current_type = Get_Type(remaining_string)
-        remaining_string = remaining_string[3:]
-
-    
+    binary_string = Input_to_List('Day_16/Input_16.txt')
+    version_sum = Recursive_Packet(binary_string)
+    print(version_sum)
 
 
 def Input_to_List(path):
@@ -51,41 +42,45 @@ def Input_to_List(path):
     for char in line:
         new_list.append(convert[char])
 
-    binary_string = str(new_list)
+    binary_string = ''
+    for char in new_list:
+        binary_string += char
             
     return binary_string
 
+
+def Bin_to_Dec(string):
+    decimal_version = 0
+    for index, value in enumerate(reversed(string)):
+        decimal_version += (2 ** index) * int(value)
+    return decimal_version
 
 
 def Get_Version(packet):
     binary_version = packet[:3]
 
-    decimal_version = 0
-    for index, value in enumerate(reversed(binary_version)):
-        decimal_version += (2 ** index) * int(value)
+    decimal_version = Bin_to_Dec(binary_version)
 
     return decimal_version
     
 
-
 def Get_Type(packet):
     binary_type = packet[3:6]
 
-    decimal_type = 0
-    for index, value in enumerate(reversed(binary_type)):
-        decimal_type += (2 ** index) * int(value)
+    decimal_type = Bin_to_Dec(binary_type)
 
     return decimal_type
 
 
-def Get_Length(packet):
-    if packet == '0':
+def Get_Length_Type(packet):
+    if packet[6] == '0':
         return 15
     else:
         return 11
 
 
 def Literal(packet):
+    version = Get_Version(packet)
     start = 6
     end = 11
     packet_number = 1
@@ -96,15 +91,115 @@ def Literal(packet):
     while subpacket[0] == '1':
         data.append(subpacket[1:])
         packet_number += 1
-        subpacket = packet[start*packet_number:end*packet_number]
+        subpacket = packet[start+5*(packet_number-1):end+5*(packet_number-1)]
 
     data.append(subpacket[1:])
+
+    endpoint = 6 + 5*packet_number
+    # while endpoint % 4 != 0:
+    #     endpoint += 1
     
-    return data
+    return version, endpoint
 
 
-def Operator(packet, bits_or_packets, length):
-    current_packet = packet[:length]
+def Operator(packet):
+    version_total = Get_Version(packet)
+    length_type = Get_Length_Type(packet)
+    
+    #total length of subpackets in bits
+    if (length_type == 15):
+        bit_length = Bin_to_Dec(packet[7:22])
+        version, endpoint = Parse_Subpackets_bits(packet[22:22+bit_length])
+        version_total += version
+        endpoint_total = 22 + endpoint
+
+    #total number of subpackets immediately contained in this packet
+    else:
+        num_subpackets = Bin_to_Dec(packet[7:18])
+        version, endpoint = Parse_Subpackets_Count(packet[18:], num_subpackets)
+        version_total += version
+        endpoint_total = 18 + endpoint
+
+    return version_total, endpoint_total
+
+
+def Parse_Subpackets_bits(packet):
+    version_total = 0
+    endpoint_total = 0
+    end_points = []
+    versions = []
+    types = []
+    
+    while packet:
+        current_type = Get_Type(packet)
+        types.append(current_type)
+        if current_type == 4:
+            version, endpoint = Literal(packet)
+            versions.append(version)
+        else:
+            version, endpoint = Operator(packet)
+            versions.append(version)
+        
+        end_points.append(endpoint)
+        packet = packet[endpoint:]
+
+    
+    for term in versions:
+        version_total += term
+    
+    for term in end_points:
+        endpoint_total += term
+
+    return version_total, endpoint_total
+
+
+def Parse_Subpackets_Count(packet, num_packets):
+    version_total = 0
+    endpoint_total = 0
+    packet_count = 0
+    end_points = []
+    versions = []
+    types = []
+    
+    while packet and packet_count < num_packets:
+        current_type = Get_Type(packet)
+        types.append(current_type)
+        if current_type == 4:
+            version, endpoint = Literal(packet)
+            versions.append(version)
+        else:
+            version, endpoint = Operator(packet)
+            versions.append(version)
+
+        packet_count += 1
+        
+        end_points.append(endpoint)
+        packet = packet[endpoint:]
+
+
+    for term in versions:
+        version_total += term
+
+    for term in end_points:
+        endpoint_total += term
+
+    return version_total, endpoint_total
+
+
+def Recursive_Packet(packet):
+    version_sum = 0
+    
+    current_type = Get_Type(packet)
+    if current_type == 4:
+        version_total, endpoint = Literal(packet)
+        version_sum += version_total
+    else:
+        version_total, endpoint = Operator(packet)
+        version_sum += version_total
+
+    return version_sum
+
+
 
 
 if __name__ == '__main__':
--- AOC_2021/Day_16/Solution_16_p2.py
+++ AOC_2021/Day_16/Solution_16_p2.py
@@ -3,93 +3,205 @@ import copy
 
 
 def main():
-    chiton_grid = Input_to_List('Day_15/Input_15.txt')
-    expanded_grid = Expand_Map(chiton_grid)
-    length = Width_Recursive_Search(expanded_grid)
-    print(length)
-    
+    binary_string = Input_to_List('Day_16/Example_p2_1_16.txt')
+    version_sum = Recursive_Packet(binary_string)
+    print(version_sum)
 
 
 def Input_to_List(path):
 
-    match_string = r"\d"
+    # match_string = r"\d"
 
     file = open(path, 'r')
+    #Treats it as hexadecimal
     line = file.readline().strip()
-    row = list(map(int,re.findall(match_string, line)))
+    # row = list(map(int,re.findall(match_string, line)))
 
-    return_list = []
+    convert = {
+        '0':'0000',
+        '1':'0001',
+        '2':'0010',
+        '3':'0011',
+        '4':'0100',
+        '5':'0101',
+        '6':'0110',
+        '7':'0111',
+        '8':'1000',
+        '9':'1001',
+        'A':'1010',
+        'B':'1011',
+        'C':'1100',
+        'D':'1101',
+        'E':'1110',
+        'F':'1111'
+    }
+
+    new_list = []
 
 
-    while True:
-        if not line:
-            break
-        else:
-            return_list.append(row)
-            line = file.readline().strip()
-            row = list(map(int,re.findall(match_string, line)))
+    for char in line:
+        new_list.append(convert[char])
+
+    binary_string = ''
+    for char in new_list:
+        binary_string += char
             
-    return return_list
+    return binary_string
 
 
-def Expand_Map(chiton_grid):
-    expanded_grid = []
-
-    for i in range(5):
-        for row_idx, row in enumerate(chiton_grid):
-            expanded_row = []
-            for j in range(5):
-                for term in row:
-                    number = term + i + j
-                    if number > 9:
-                        number -= 9
-                    expanded_row.append(number)
-            expanded_grid.append(expanded_row)
-
-    return expanded_grid
+def Bin_to_Dec(string):
+    decimal_version = 0
+    for index, value in enumerate(reversed(string)):
+        decimal_version += (2 ** index) * int(value)
+    return decimal_version
 
 
+def Get_Version(packet):
+    binary_version = packet[:3]
 
-def Width_Recursive_Search (chiton_grid):
+    decimal_version = Bin_to_Dec(binary_version)
 
-    distance_grid = [[None for _ in range(len(chiton_grid[0]))] for __ in range(len(chiton_grid))]
-    distance_grid[0][0] = 0
+    return decimal_version
+    
 
-    queue = [[0,0]]
+def Get_Type(packet):
+    binary_type = packet[3:6]
 
-    while queue:
-        coordinate = queue.pop(0)
-        #up
-        if coordinate[0] > 0:
-            up_length = distance_grid[coordinate[0]][coordinate[1]] + chiton_grid[coordinate[0]-1][coordinate[1]]
-            if distance_grid[coordinate[0]-1][coordinate[1]] == None or up_length < distance_grid[coordinate[0]-1][coordinate[1]]:
-                distance_grid[coordinate[0]-1][coordinate[1]] = up_length
-                queue.append([coordinate[0]-1,coordinate[1]])
-        #down
-        if coordinate[0] < len(chiton_grid)-1:
-            down_length = distance_grid[coordinate[0]][coordinate[1]] + chiton_grid[coordinate[0]+1][coordinate[1]]
-            if distance_grid[coordinate[0]+1][coordinate[1]] == None or down_length < distance_grid[coordinate[0]+1][coordinate[1]]:
-                distance_grid[coordinate[0]+1][coordinate[1]] = down_length
-                queue.append([coordinate[0]+1,coordinate[1]])
-        #left
-        if coordinate[1] > 0:
-            left_length = distance_grid[coordinate[0]][coordinate[1]] + chiton_grid[coordinate[0]][coordinate[1]-1]
-            if distance_grid[coordinate[0]][coordinate[1]-1] == None or left_length < distance_grid[coordinate[0]][coordinate[1]-1]:
-                distance_grid[coordinate[0]][coordinate[1]-1] = left_length
-                queue.append([coordinate[0],coordinate[1]-1])
-        #right
-        if coordinate[1] < len(chiton_grid[0])-1:
-            right_length = distance_grid[coordinate[0]][coordinate[1]] + chiton_grid[coordinate[0]][coordinate[1]+1]
-            if distance_grid[coordinate[0]][coordinate[1]+1] == None or right_length < distance_grid[coordinate[0]][coordinate[1]+1]:
-                distance_grid[coordinate[0]][coordinate[1]+1] = right_length
-                queue.append([coordinate[0],coordinate[1]+1])
+    decimal_type = Bin_to_Dec(binary_type)
 
-    return distance_grid[len(chiton_grid)-1][len(chiton_grid[0])-1]
+    return decimal_type
+
+
+def Get_Length_Type(packet):
+    if packet[6] == '0':
+        return 15
+    else:
+        return 11
+
+
+def Literal(packet):
+    version = Get_Version(packet)
+    start = 6
+    end = 11
+    packet_number = 1
+    subpacket = packet[start:end]
+
+    data = []
+
+    while subpacket[0] == '1':
+        data.append(subpacket[1:])
+        packet_number += 1
+        subpacket = packet[start+5*(packet_number-1):end+5*(packet_number-1)]
+
+    data.append(subpacket[1:])
+
+    endpoint = 6 + 5*packet_number
+    # while endpoint % 4 != 0:
+    #     endpoint += 1
+    
+    return version, endpoint
+
+
+def Operator(packet):
+    version_total = Get_Version(packet)
+    length_type = Get_Length_Type(packet)
+    
+    #total length of subpackets in bits
+    if (length_type == 15):
+        bit_length = Bin_to_Dec(packet[7:22])
+        version, endpoint = Parse_Subpackets_bits(packet[22:22+bit_length])
+        version_total += version
+        endpoint_total = 22 + endpoint
+
+    #total number of subpackets immediately contained in this packet
+    else:
+        num_subpackets = Bin_to_Dec(packet[7:18])
+        version, endpoint = Parse_Subpackets_Count(packet[18:], num_subpackets)
+        version_total += version
+        endpoint_total = 18 + endpoint
+
+    return version_total, endpoint_total
+
+
+def Parse_Subpackets_bits(packet):
+    version_total = 0
+    endpoint_total = 0
+    end_points = []
+    versions = []
+    types = []
+    
+    while packet:
+        current_type = Get_Type(packet)
+        types.append(current_type)
+        if current_type == 4:
+            version, endpoint = Literal(packet)
+            versions.append(version)
+        else:
+            version, endpoint = Operator(packet)
+            versions.append(version)
+        
+        end_points.append(endpoint)
+        packet = packet[endpoint:]
+
+    
+    for term in versions:
+        version_total += term
+    
+    for term in end_points:
+        endpoint_total += term
+
+    return version_total, endpoint_total
+
+
+def Parse_Subpackets_Count(packet, num_packets):
+    version_total = 0
+    endpoint_total = 0
+    packet_count = 0
+    end_points = []
+    versions = []
+    types = []
+    
+    while packet and packet_count < num_packets:
+        current_type = Get_Type(packet)
+        types.append(current_type)
+        if current_type == 4:
+            version, endpoint = Literal(packet)
+            versions.append(version)
+        else:
+            version, endpoint = Operator(packet)
+            versions.append(version)
+
+        packet_count += 1
+        
+        end_points.append(endpoint)
+        packet = packet[endpoint:]
+
+
+    for term in versions:
+        version_total += term
+
+    for term in end_points:
+        endpoint_total += term
+
+    return version_total, endpoint_total
+
+
+def Recursive_Packet(packet):
+    version_sum = 0
+    
+    current_type = Get_Type(packet)
+    if current_type == 4:
+        version_total, endpoint = Literal(packet)
+        version_sum += version_total
+    else:
+        version_total, endpoint = Operator(packet)
+        version_sum += version_total
+
+    return version_sum
+
+
 
 
 if __name__ == '__main__':
     main()
-
-
-
 
